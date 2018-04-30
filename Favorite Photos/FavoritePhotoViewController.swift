3b856a204adec103a1fa1fc1bdc1fb49ed08cee3
Ryan Greenlee <ryan.greenlee1@gmail.com>
2018-04-30 12:21:12 -0400

    User interface setup

---
+++ Favorite Photos/FavoritePhotoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FavoritePhotoViewController.swift
+//  Favorite Photos
+//
+//  Created by Ryan Greenlee on 4/30/18.
+//  Copyright © 2018 Ryan Greenlee. All rights reserved.
+//
+
+import UIKit
+
+class FavoritePhotoViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        
+    }
+}
